f46884c96bb2228955e8130f7058b9c2b74ffd64
Robert Rousset <whyisrob@hotmail.com>
2021-06-14 22:50:00 +0800

    got blog posts to display on homepage

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { User, Blog } = require("../models");
 
-router.get('/', async (req, res) => {
-    res.render('home')
-})
+router.get("/", async (req, res) => {
+  const blogPosts = await Blog.findAll();
+  const blogs = blogPosts.map((element) => element.get({ plain: true }));
+  console.log(blogs);
+  res.render("home", { blogs });
+});
 
 module.exports = router;
